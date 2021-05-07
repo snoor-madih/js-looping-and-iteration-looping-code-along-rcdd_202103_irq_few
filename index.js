e1f14eed39538d682e7f1e4c8faf83f05756508a
snoor_madih <snoor.madih@gmail.com>
2021-05-07 18:34:39 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,3 +19,19 @@
 //   }
 // }
 // countDown(10);
+
+function writeCards( namesArray, event ) {
+  let thankYouCards = []
+  for ( let i = 0; i < namesArray.length; i++ ) {
+    thankYouCards.push( `Thank you, ${namesArray[i]}, for the wonderful ${event} gift!` )
+  }
+  return thankYouCards
+}
+
+function countDown( startingNumber ) {
+  while ( startingNumber > 0 ) {
+    console.log( startingNumber );
+    startingNumber -= 1;
+  }
+  console.log( startingNumber );
+}
