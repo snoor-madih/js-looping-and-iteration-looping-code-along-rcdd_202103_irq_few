fdbb67590ac953d9be349b36e06b9d59d5d5477e
snoor_madih <snoor.madih@gmail.com>
2021-05-07 18:34:37 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,21 +1,21 @@
-// Code your solutions in th
-let names;
-const event;
-function writeCards(names, event)
-{
-for (let i=0 ; i< writeCards.length;i++)
-{
-  console.log ("thank you 4{names[i],for wonderful ${event} gift!");
-}
-}
-writeCards(["Ada", "Brendan", "Ali"], "birthday");
-function countDown(num)
-{
-  let i =1;
-  while(i>=num)
-  {
-    console.log(i);
-    i--;
-  }
-}
-countDown(10);
+// // Code your solutions in th
+// let names;
+// const event;
+// function writeCards(names, event)
+// {
+// for (let i=0 ; i< writeCards.length;i++)
+// {
+//   console.log ("thank you 4{names[i],for wonderful ${event} gift!");
+// }
+// }
+// writeCards(["Ada", "Brendan", "Ali"], "birthday");
+// function countDown(num)
+// {
+//   let i =1;
+//   while(i>=num)
+//   {
+//     console.log(i);
+//     i--;
+//   }
+// }
+// countDown(10);
