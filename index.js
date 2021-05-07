2a0c0df2f9f9d820614f4cefe0123444467d26d2
snoor_madih <snoor.madih@gmail.com>
2021-05-07 18:35:01 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,25 +1,3 @@
-// // Code your solutions in th
-// let names;
-// const event;
-// function writeCards(names, event)
-// {
-// for (let i=0 ; i< writeCards.length;i++)
-// {
-//   console.log ("thank you 4{names[i],for wonderful ${event} gift!");
-// }
-// }
-// writeCards(["Ada", "Brendan", "Ali"], "birthday");
-// function countDown(num)
-// {
-//   let i =1;
-//   while(i>=num)
-//   {
-//     console.log(i);
-//     i--;
-//   }
-// }
-// countDown(10);
-
 function writeCards( namesArray, event ) {
   let thankYouCards = []
   for ( let i = 0; i < namesArray.length; i++ ) {
